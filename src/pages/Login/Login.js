d016f625d3d2cdf00f8bd8003e46e4368fc25e58
avikmajumder17 <avikmajumder17@gmail.com>
2026-05-04 14:49:36 +0530

    initial commit

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
-
 import axios from "axios";
+
+import { Loader } from "../../components/Loader/Loader";
 
 
 import "./Login.css";
 
 
 
-
 export const Login = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+
+
   const handleLogin = async (e) => {
     e.preventDefault();
 
@@ -19,6 +24,8 @@ export const Login = () => {
     };
 
     try {
+      setIsLoading(true);
+
       await axios.post(
         "https://sign-log-in-backend.onrender.com/api/v1/users/log-in",
         userDetails
@@ -29,53 +36,59 @@ export const Login = () => {
       e.target.reset();
     } catch (err) {
       toast.error(err.message || "Error occured");
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
-    <div className="login-wrapper container d-flex justify-content-center align-items-center vh-100">
-      <div
-        className="card glass-card shadow p-4"
-        style={{ width: "100%", maxWidth: 400 }}
-      >
-        <h3 className="text-center mb-4">Login</h3>
-        <form onSubmit={handleLogin}>
-          {/* Email */}
-          <div className="mb-3">
-            <label className="form-label">Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              placeholder="Enter email"
-              name="email"
-              required=""
-            />
-          </div>
-          {/* Password */}
-          <div className="mb-3">
-            <label className="form-label">Password</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Enter password"
-              name="password"
-              required=""
-            />
-          </div>
-          {/* Remember Me */}
-          <div className="mb-3 form-check">
-            <input type="checkbox" className="form-check-input" />
-            <label className="form-check-label">Remember me</label>
-          </div>
-          {/* Submit */}
-          <button type="submit" className="btn btn-primary w-100">
-            Login
-          </button>
-        </form>
-        <p className="text-center mt-3">
-          Don't have an account? <Link to="/register" className="text-white">Register</Link>
-        </p>
+    <>
+      { isLoading && <Loader /> }
+
+      <div className="login-wrapper container d-flex justify-content-center align-items-center vh-100">
+        <div
+          className="card glass-card shadow p-4"
+          style={{ width: "100%", maxWidth: 400 }}
+        >
+          <h3 className="text-center mb-4">Login</h3>
+          <form onSubmit={handleLogin}>
+            {/* Email */}
+            <div className="mb-3">
+              <label className="form-label">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+                name="email"
+                required=""
+              />
+            </div>
+            {/* Password */}
+            <div className="mb-3">
+              <label className="form-label">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                name="password"
+                required=""
+              />
+            </div>
+            {/* Remember Me */}
+            <div className="mb-3 form-check">
+              <input type="checkbox" className="form-check-input" />
+              <label className="form-check-label">Remember me</label>
+            </div>
+            {/* Submit */}
+            <button type="submit" className="btn btn-primary w-100">
+              Login
+            </button>
+          </form>
+          <p className="text-center mt-3">
+            Don't have an account? <Link to="/register" className="text-white">Register</Link>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
-
 import axios from "axios";
 
 
